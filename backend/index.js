18283826df3bd16bd50a677f9ec1d57afa4bdb4d
Aqil Wafi <m.aqil.wafi@gmail.com>
2025-11-19 10:19:28 +0700

    update 2

--- backend/index.js
+++ backend/index.js
@@ -82,7 +82,7 @@ if (process.env.NODE_ENV === 'production') {
   const frontendPath = path.join(__dirname, 'public');
   app.use(express.static(frontendPath));
 
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
   });
 }
